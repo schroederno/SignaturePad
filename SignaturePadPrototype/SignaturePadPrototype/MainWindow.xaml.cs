240cc07d30374740d728c2f119d9993981c2da67
a-redeker <aredeker@practicevelocity.com>
2017-06-27 08:53:00 -0500

    There ya go

--- SignaturePadPrototype/SignaturePadPrototype/MainWindow.xaml.cs
+++ SignaturePadPrototype/SignaturePadPrototype/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using System.Net;
 using System.Windows;
 using System.Windows.Input;
@@ -16,7 +17,7 @@ namespace SignaturePadPrototype
     public partial class MainWindow : DXWindow
     {
         System.Windows.Point currentPoint = new System.Windows.Point(0,0);
-        System.Windows.Point endPoint = new System.Windows.Point(0,0);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,38 +30,60 @@ namespace SignaturePadPrototype
             //    documentProcessor.SaveDocument("C:\\Users\\nschroeder\\Documents\\Result.pdf");
             //}
         }
-        static void Draw(PdfPage page, PdfDocumentProcessor processor, PdfDocumentPosition position, float z)
+        static void Draw(PdfPage page, PdfDocumentProcessor processor, System.Windows.Point position, float z)
         {
-            using (DevExpress.Pdf.PdfGraphics graphics = processor.CreateGraphics())
+            using (PdfGraphics graphics = processor.CreateGraphics())
             {
-                using (Bitmap image = new Bitmap("C:\\Users\\nschroeder\\Pictures\\download.png"))
+                using (Bitmap image = new Bitmap("C:\\Users\\aredeker\\Pictures\\Slack\\Capture.png"))
                 {
                     float width = image.Width;
                     float height = image.Height;
-                    graphics.DrawImage(image, new RectangleF((float)position.Point.X, z+110-(float)position.Point.Y, width/2,height/2));
+                    graphics.DrawImage(image, new RectangleF((float)position.X + 200, (float)position.Y, width/2,height/2));
                 }
                 graphics.AddToPageForeground(page);
             }
         }
-        private void PdfViewerControl_OnSelectionStarted(DependencyObject d, SelectionEventArgs e)
-        {
-            currentPoint = PointToScreen(currentPoint);
-            //currentPoint = Mouse.GetPosition(Control);
-        }
+       // private void PdfViewerControl_OnSelectionStarted(DependencyObject d, SelectionEventArgs e)
+       // {
+       //     currentPoint = PointToScreen(currentPoint);
+       //     //currentPoint = Mouse.GetPosition(Control);
+       // }
 
-        private void Control_OnSelectionEnded(DependencyObject d, SelectionEventArgs e)
-        {
+       // private void Control_OnSelectionEnded(DependencyObject d, SelectionEventArgs e)
+       // {
             
-            //endPoint = PointToScreen(endPoint);
-            //PdfDocumentPosition position = Control.ConvertPixelToDocumentPosition(endPoint);
+       //     //endPoint = PointToScreen(endPoint);
+       //     //PdfDocumentPosition position = Control.ConvertPixelToDocumentPosition(endPoint);
+       //     using (PdfDocumentProcessor documentProcessor = new PdfDocumentProcessor())
+       //     {
+       //         documentProcessor.LoadDocument("C:\\Users\\aredeker\\Documents\\Sample Docs\\SampleCreditCardPre-Authorizationform.pdf");
+       //         PdfPage page = documentProcessor.Document.Pages[Control.CurrentPageNumber-1];
+       //         double z = page.CropBox.Height;
+       //         Draw(page, documentProcessor, e.DocumentPosition.Point., (float)z);
+       //         documentProcessor.SaveDocument("C:\\Users\\aredeker\\Documents\\Result.pdf");
+       //     }
+       //}
+
+
+        public void Control_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            //MessageBox.Show(string.Format("X: {0} Y: {1}", MousePosition.X, MousePosition.Y));
+            var postion = e.GetPosition((IInputElement) sender);
+            //MessageBox.Show(postion.ToString());
+
             using (PdfDocumentProcessor documentProcessor = new PdfDocumentProcessor())
             {
-                documentProcessor.LoadDocument("C:\\Users\\nschroeder\\Documents\\EmailDownloads\\SampleCreditCardPre-Authorizationform.pdf");
-                PdfPage page = documentProcessor.Document.Pages[Control.CurrentPageNumber-1];
+                documentProcessor.LoadDocument("C:\\Users\\aredeker\\Documents\\Sample Docs\\SampleCreditCardPre-Authorizationform.pdf");
+                PdfPage page = documentProcessor.Document.Pages[Control.CurrentPageNumber - 1];
                 double z = page.CropBox.Height;
-                Draw(page, documentProcessor, e.DocumentPosition, (float)z);
-                documentProcessor.SaveDocument("C:\\Users\\nschroeder\\Documents\\Result.pdf");
+                postion.X = page.CropBox.Width - postion.X;
+                Draw(page, documentProcessor, postion, (float)z);
+                documentProcessor.SaveDocument("C:\\Users\\aredeker\\Documents\\Result.pdf");
+                Process.Start("C:\\Users\\aredeker\\Documents\\Result.pdf");
+
             }
-       }
+
+        }
     }
 }
